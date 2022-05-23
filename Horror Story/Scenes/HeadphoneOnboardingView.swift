d5f92c90e0d6506c56df5defb6ccacf782b26d2c
Guilerme Barciki <guilerme.barciki@snowmanlabs.com>
2022-05-22 23:05:57 -0300

    finish headphone onboarding

--- Horror Story/Scenes/HeadphoneOnboardingView.swift
+++ Horror Story/Scenes/HeadphoneOnboardingView.swift
@@ -12,30 +12,84 @@ class HeadPhoneOnboardingView: UIView {
     lazy var infoLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Para uma melhor experiência,\nuse fones com Spatial Audio"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        label.numberOfLines = 0
+        label.textColor = .white
+        return label
+    }()
+    
+    lazy var subInfoLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "AirPods Pro, AirPods Max ou similares"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        label.numberOfLines = 0
+        label.textColor = .white
         return label
     }()
     
     lazy var headPhonesImage: UIImageView = {
         let image = UIImageView()
-        image.image = UIImage(named: "")
+        image.image = UIImage(named: "headphones-onboarding")
+        image.contentMode = .scaleAspectFit
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
+    }()
+    
+    lazy var spacer = UIStackView()
+    
+    lazy var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Entendi", for: .normal)
+        button.setTitleColor(UIColor.systemBlue, for: .normal)
+        return button
     }()
     
     lazy var stackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 12
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
         return stackView
     }()
     
     override init(frame: CGRect) {
-        super.init(frame: .zero)
+        super.init(frame: CGRect(x: 0, y: 0, width: 270, height: 325))
         setupUI()
     }
     
     private func setupUI() {
         addSubview(stackView)
-        NSLayoutConstraint.activate(<#T##constraints: [NSLayoutConstraint]##[NSLayoutConstraint]#>)
+        self.layer.cornerRadius = 15
+        self.layer.masksToBounds = true
+        self.backgroundColor = .black
+        self.layer.opacity = 0.75
+        self.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.heightAnchor.constraint(equalToConstant: 325),
+            self.widthAnchor.constraint(equalToConstant: 270)
+        ])
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
+            stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -20),
+            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 14),
+            stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -14)
+        ])
+        stackView.addArrangedSubview(infoLabel)
+        stackView.addArrangedSubview(subInfoLabel)
+        stackView.addArrangedSubview(spacer)
+        stackView.addArrangedSubview(headPhonesImage)
+        stackView.addArrangedSubview(button)
+        button.addTarget(self, action: #selector(buttonPressed(sender:)), for: .touchUpInside)
+    }
+    
+    @objc func buttonPressed(sender: UIButton) {
+        self.removeFromSuperview()
     }
     
     required init?(coder: NSCoder) {
